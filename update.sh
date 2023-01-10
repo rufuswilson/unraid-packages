623533ea0064c9e03de2d24c157dd92b49abb68e
slackdev <slackdev@slackdev.local>
2023-01-10 19:12:46 +0100

    finished update script

--- update.sh
+++ update.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
 NERDTOOL="../unRAID-NerdTools"
+NERDPKG="${NERDTOOL}/packages/"
 
-
-# Absolute path to this script, e.g. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f "$0")
-# Absolute path this script is in, thus /home/user/bin
-SCRIPTPATH=$(dirname "$SCRIPT")
+SCRIPTPATH=$(dirname "${SCRIPT}")
 
-cd "${NERDTOOL}"
+cd "${SCRIPTPATH}/${NERDPKG}"
 
-find "${SCRIPTPATH}" -name '*.txz' -print0  | while IFS= read -r -d '' line; do
-   echo "$line"
+git pull
+
+find "${SCRIPTPATH}" -name '*.txz' -print0 | while IFS= read -r -d '' fl; do
+   fn=$(echo "${fl}" | sed -e 's#^.*/\([^/]*\)$#\1#g')
+   pn=$(echo "${fn}" | sed -e 's#^\(.*\)\-\([^\-]*\)\-\([^\-]*\).txz$#\1#g')
+   vn=$(echo "${fn}" | sed -e 's#^\(.*\)\-\([^\-]*\)\-\([^\-]*\).txz$#\2#g')
+   pt=$(echo "${fn}" | sed -e 's#^\(.*\)\-\([^\-]*\)\-\([^\-]*\).txz$#\3#g')
+   echo "Processing ${pn} ${vn} ${pt}"
+   if [[ -n $(find . -name "${fn}") ]] ; then
+      echo "   already up-to-date"
+      continue
+   else
+      find . -name "${pn}*{pt}.txz" -exec rm {} \;
+      for D in */; do
+         echo "   copying to ${D}"
+         cp "${fl}" "${SCRIPTPATH}/${NERDPKG}${D}"
+         git add "${SCRIPTPATH}/${NERDPKG}${D}"
+      done
+   fi
+   git commit -m "Updated ${pn} to ${vn} for ${pt}"
 done
+
+git push
+
