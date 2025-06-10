2d309e020148d955cedd1f110da8ddb00631ed7c
Rufus Wilson <marsou75@gmail.com>
2025-06-10 08:45:10 +0200

    Finished to setup autocompile

--- Slackbuild/autocompile.sh
+++ Slackbuild/autocompile.sh
@@ -18,52 +18,59 @@ for d in ${SCRIPTPATH}/*/ ; do
    echo "  ${PACKAGE}"
 
    bash "${d}/update.sh" > "${d}/update.log"
-   if [ "$?" != 0 ]; then
-      echo "     Already up to date"
-      continue
-   fi
+   # if [ "$?" != 0 ]; then
+      # echo "     Already up to date"
+      # continue
+   # fi
 
    echo "     Preparing subfolders"
    if [ ! -d "${d}/build-deps" ] ; then
       mkdir "${d}/build-deps"
    else
-      rm "${d}/build-deps/"*.* 2>/dev/null
+      rm "${d}/build-deps/"*.txz 2>/dev/null
    fi
-   if [ ! -d "${d}/output" ] ; then
-      mkdir "${d}/output"
-   else
-      rm "${d}/output/"*.{log} 2>/dev/null
-   fi
+   # if [ ! -d "${d}/output" ] ; then
+      # mkdir "${d}/output"
+   # else
+      # rm "${d}/output/"*.{log} 2>/dev/null
+      # rm "${d}/output/"*.txz 2>/dev/null
+   # fi
 
    echo "     Retrieving dependencies"
    DEPS="$(cat "${d}/requirements.txt" | tr -s '\n' ' ')"
-   bash "${SCRIPTPATH}/pkgdl" download "${d}/build-deps" ${DEPS} > "${d}/output/deps.log"
+   # bash "${SCRIPTPATH}/pkgdl" download "${d}/build-deps" ${DEPS} > "${d}/output/deps.log"
 
    echo "     Getting version"
    VERSIONNEW=$(cat "${d}/slackbuild/version" | tr -d '\n')
 
    echo "     Building package"
-   docker run \
-      --rm \
-      --name SBR_${PACKAGE}_${VERSIONNEW} \
-      --mount type=bind,src=${d}/build-deps,dst=/build-deps \
-      --mount type=bind,src=${d}/slackbuild,dst=/slackbuild \
-      --mount type=bind,src=${d}/output,dst=/output \
-      -e OUTPUT=/output \
-      -e TMP=/tmp \
-      -it --entrypoint /bin/bash \
-      ghcr.io/lanjelin/slackbuilder:latest \
-      > "${d}/output/compile.log"
+   # docker run \
+      # --rm \
+      # --name SBR_${PACKAGE}_${VERSIONNEW} \
+      # --mount type=bind,src=${d}/build-deps,dst=/build-deps \
+      # --mount type=bind,src=${d}/slackbuild,dst=/slackbuild \
+      # --mount type=bind,src=${d}/output,dst=/output \
+      # -e OUTPUT=/output \
+      # -e TMP=/tmp \
+      # ghcr.io/lanjelin/slackbuilder:latest \
+      # > "${d}/output/compile.log"
       # -it --entrypoint /bin/bash \
 
-   # if [ ! -d "${SCRIPTPATH}/../slackware64-current/${PACKAGE}" ] ; then
-   #    mkdir "${SCRIPTPATH}/../slackware64-current/${PACKAGE}"
-   # fi
+   echo "     Copying package to repo"
+   if [ ! -d "${SCRIPTPATH}/../slackware64-current" ] ; then
+      mkdir "${SCRIPTPATH}/../slackware64-current"
+   fi
+   if [ ! -d "${SCRIPTPATH}/../slackware64-current/${PACKAGE}" ] ; then
+      mkdir "${SCRIPTPATH}/../slackware64-current/${PACKAGE}"
+   fi
+   cp "${d}/output/"*.* "${SCRIPTPATH}/../slackware64-current/${PACKAGE}/"
+   rm "${SCRIPTPATH}/../slackware64-current/${PACKAGE}/"*.log
 
-   # echo "     Comitting changes"
-   # git add "${d}/slackbuild/version"
-   # git add "${SCRIPTPATH}/../slackware64-current/${PACKAGE}/*"
-   # git commit -m "updated version for ${PACKAGE}"
+   echo "     Comitting changes"
+   git add "${d}/slackbuild/version"
+   find "${SCRIPTPATH}/../slackware64-current/${PACKAGE}/" -type f ! -name "*${VERSIONNEW}*" -exec git rm {} \;
+   git add "${SCRIPTPATH}/../slackware64-current/${PACKAGE}/"${PACKAGE}*${VERSIONNEW}*.*
+   git commit -m "${PACKAGE} updated to ${VERSIONNEW}"
 done
 
 echo "Compilation finished"
