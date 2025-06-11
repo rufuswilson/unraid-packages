304ab0376e67c24bc761c4d7f4c12f68e208dab2
Rufus Wilson <marsou75@gmail.com>
2025-06-12 00:50:43 +0200

    Improved failure detections and git support

--- Slackbuild/autocompile.sh
+++ Slackbuild/autocompile.sh
@@ -4,9 +4,13 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "${SCRIPT}")
 SCRIPTNAME=$(basename -- "${SCRIPT}")
 
-# Update repositories
+echo "Update repositories"
 bash "${SCRIPTPATH}/pkgdl" update
 
+echo "Update slackbuilder docker image"
+docker pull ghcr.io/lanjelin/slackbuilder:latest
+
+hasUpdate=0
 echo "Starting compilations"
 for d in ${SCRIPTPATH}/*/ ; do
    d="$(echo "${d}" | sed -e 's#/$##')"
@@ -17,9 +21,16 @@ for d in ${SCRIPTPATH}/*/ ; do
    
    echo "  ${PACKAGE}"
 
-   bash "${d}/update.sh" > "${d}/update.log"
+   if [ ! -d "${d}/logs" ] ; then
+      mkdir "${d}/logs"
+   else
+      rm -f "${d}/logs/"*.* 2>/dev/null
+   fi
+
+   echo "     Checking version"
+   bash "${d}/update.sh" > "${d}/logs/update.log"
    if [ "$?" != 0 ]; then
-      echo "     Already up to date"
+      echo "          Already up to date"
       continue
    fi
 
@@ -27,18 +38,21 @@ for d in ${SCRIPTPATH}/*/ ; do
    if [ ! -d "${d}/build-deps" ] ; then
       mkdir "${d}/build-deps"
    else
-      rm "${d}/build-deps/"*.txz 2>/dev/null
+      rm -f "${d}/build-deps/"*.{tbz,tgz,tlz,txz} 2>/dev/null
    fi
    if [ ! -d "${d}/output" ] ; then
       mkdir "${d}/output"
    else
-      rm "${d}/output/"*.{log} 2>/dev/null
-      rm "${d}/output/"*.txz 2>/dev/null
+      rm -f "${d}/output/"*.* 2>/dev/null
    fi
 
    echo "     Retrieving dependencies"
    DEPS="$(cat "${d}/requirements.txt" | tr -s '\n' ' ')"
-   bash "${SCRIPTPATH}/pkgdl" download "${d}/build-deps" ${DEPS} > "${d}/output/deps.log"
+   bash "${SCRIPTPATH}/pkgdl" download "${d}/build-deps" ${DEPS} > "${d}/logs/deps.log"
+   if [ "$?" != 0 ]; then
+      echo "          xxx failed"
+      exit 1
+   fi
 
    echo "     Getting version"
    VERSIONNEW=$(cat "${d}/slackbuild/version" | tr -d '\n')
@@ -50,11 +64,17 @@ for d in ${SCRIPTPATH}/*/ ; do
       --mount type=bind,src=${d}/build-deps,dst=/build-deps \
       --mount type=bind,src=${d}/slackbuild,dst=/slackbuild \
       --mount type=bind,src=${d}/output,dst=/output \
+      --mount type=bind,src=${d}/logs,dst=/logs \
       -e OUTPUT=/output \
       -e TMP=/tmp \
-      ghcr.io/lanjelin/slackbuilder:latest \
-      > "${d}/output/compile.log"
+      -e VERBOSITY=1 \
+      ghcr.io/lanjelin/slackbuilder:latest
       # -it --entrypoint /bin/bash \
+   
+   if [ -z "$( ls -A '${d}/output/' )" ]; then
+      echo "          xx Failed to build"
+      continue
+   fi
 
    echo "     Copying package to repo"
    if [ ! -d "${SCRIPTPATH}/../slackware64-current" ] ; then
@@ -64,18 +84,19 @@ for d in ${SCRIPTPATH}/*/ ; do
       mkdir "${SCRIPTPATH}/../slackware64-current/${PACKAGE}"
    fi
    rm "${SCRIPTPATH}/../slackware64-current/${PACKAGE}/"*.*
-   cp "${d}/output/"*.* "${SCRIPTPATH}/../slackware64-current/${PACKAGE}/"
-   rm "${SCRIPTPATH}/../slackware64-current/${PACKAGE}/"*.log
+   cp "${d}/output/"*${VERSIONNEW}*.{tbz,tgz,tlz,txz} "${SCRIPTPATH}/../slackware64-current/${PACKAGE}/"
 
    echo "     Staging changes"
    git add "${d}/slackbuild/version"
    git add -A "${SCRIPTPATH}/../slackware64-current/${PACKAGE}/"
+   hasUpdate=1
 done
 
-if [ ! -z "$(git status -s | grep '^[MTADRCU]')" ] ; then
-	bash "${SCRIPTPATH}/../slackware64-current/buildlist.sh" "${SCRIPTPATH}/../slackware64-current/"
-	git add "${SCRIPTPATH}/../slackware64-current/"
-	git commit -m "Packages updated"
+if [[ ${hasUpdate} -eq 1 ]] ; then
+    cd "${SCRIPTPATH}/../slackware64-current"
+    bash "buildlist.sh" "./"
+    git add ./
+    git commit -m "Packages updated"
 fi
 
 echo "Compilation finished"
