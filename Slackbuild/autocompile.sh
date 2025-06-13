f28305a4970f05b9707c2ebda7f56dbaceb8651f
rufuswilson <marsou75@gmail.com>
2025-06-13 09:24:35 +0200

    cleaned up logging

--- Slackbuild/autocompile.sh
+++ Slackbuild/autocompile.sh
@@ -4,8 +4,10 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "${SCRIPT}")
 SCRIPTNAME=$(basename -- "${SCRIPT}")
 
-echo "Update current repository"
-git pull > "${SCRIPTPATH}/git.log"
+rm "${SCRIPTPATH}/"*.log 2>&1 >/dev/null
+
+echo "Update current repository" | tee -a "${SCRIPTPATH}/git.log"
+git pull >> "${SCRIPTPATH}/git.log"
 
 echo "Update repositories"
 bash "${SCRIPTPATH}/pkgdl" update 2>&1 > "${SCRIPTPATH}/pkgdl.log"
@@ -28,7 +30,7 @@ for d in ${SCRIPTPATH}/*/ ; do
    if [[ ! -d "logs" ]] ; then
       mkdir "logs"
    else
-      rm -f "logs/"*.* 2>/dev/null
+      rm -f "logs/"*.* 2>&1 1>/dev/null
    fi
 
    echo "     Checking version"
@@ -42,12 +44,12 @@ for d in ${SCRIPTPATH}/*/ ; do
    if [[ ! -d "build-deps" ]] ; then
       mkdir "build-deps"
    else
-      rm -f "build-deps/"*.{tbz,tgz,tlz,txz} 2>/dev/null
+      rm -f "build-deps/"*.{tbz,tgz,tlz,txz} 2>&1 1>/dev/null
    fi
    if [[ ! -d "output" ]] ; then
       mkdir "output"
    else
-      rm -f "output/"*.* 2>/dev/null
+      rm -f "output/"*.* 2>&1 >/dev/null
    fi
 
    echo "     Retrieving dependencies"
@@ -87,7 +89,7 @@ for d in ${SCRIPTPATH}/*/ ; do
    if [[ ! -d "${SCRIPTPATH}/../slackware64-current/${PACKAGE}" ]] ; then
       mkdir "${SCRIPTPATH}/../slackware64-current/${PACKAGE}"
    fi
-   rm "${SCRIPTPATH}/../slackware64-current/${PACKAGE}/"*.*
+   rm "${SCRIPTPATH}/../slackware64-current/${PACKAGE}/"*.* 2>/dev/null
    cp "output/"*${VERSIONNEW}*.{tbz,tgz,tlz,txz} "${SCRIPTPATH}/../slackware64-current/${PACKAGE}/"
 
    echo "     Staging changes"
@@ -101,12 +103,14 @@ if [[ ${hasUpdate} -eq 1 ]] ; then
    
    bash buildlist.sh ./
    
-   echo "Comitting changes"
+   echo '--------------------------' >> "${SCRIPTPATH}/git.log"
+   echo "Comitting changes" | tee -a "${SCRIPTPATH}/git.log"
    git add FILE_LIST CHECKSUMS.md5
-   git commit -m "Packages updated"
+   git commit -m "Packages updated" | tee -a "${SCRIPTPATH}/git.log"
    
-   echo "Pushing changes"
-   git push
+   echo '--------------------------' >> "${SCRIPTPATH}/git.log"
+   echo "Pushing changes" | tee -a "${SCRIPTPATH}/git.log"
+   git push | tee -a "${SCRIPTPATH}/git.log"
 fi
 
 echo "Compilation finished"
